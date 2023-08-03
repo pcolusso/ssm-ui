a4a4f58feb2136ab2ac3d692c837baa850bd3efd
Paul Colusso <paulcolusso@gmail.com>
2023-08-03 11:31:07 +1000

    Initial Commit

---
+++ ssm-ui/ssm_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssm_uiApp.swift
+//  ssm-ui
+//
+//  Created by Paul Colusso on 3/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct ssm_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
